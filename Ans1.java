473e9d3c399729c90d10cc51f8a95f26597e03de
pinachibajaj <pinachibajaj@gmail.com>
2021-05-13 18:01:01 +0530

    added java program named Ans1

---
+++ Ans1.java
@@ -0,0 +1,11 @@
+
+public class Ans1{
+	public static void main(String [] args){
+		int a = 10 ;
+		int b = 100 ;
+		a = a+b ;
+		b = a-b ;
+		a = a-b ;
+		System.out.println("After swapping value of a = "  + a + " and value of b = " + b); 
+ }
+}
